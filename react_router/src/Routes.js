0db03c0603c8d5ceae30c5de338a1880b0b40c73
Jared Flomen <jaredflomen@gmail.com>
2021-02-06 13:02:49 -0500

    Create Nav

--- react_router/src/Routes.js
+++ react_router/src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
+import Nav from './Nav';
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route exact path='/' component={() => <App title="Jared's Home" />} />
         <Route
