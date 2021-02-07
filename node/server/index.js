1d45f5939c6cf48a27c4da83a30d53e77d06af1d
Jared Flomen <jaredflomen@gmail.com>
2021-02-07 15:28:22 -0500

    React & Express
    Sending basic json express backend to react frontend

--- node/server/index.js
+++ node/server/index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from node express server!' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
