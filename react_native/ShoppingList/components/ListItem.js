6cadad9919b13fd9704ec53bed3b88d8068b4865
Jared Flomen <jaredflomen@gmail.com>
2021-02-21 14:59:27 -0500

    List Item Remove

--- react_native/ShoppingList/components/ListItem.js
+++ react_native/ShoppingList/components/ListItem.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const ListItem = ({ item }) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
+        <FontAwesome name='remove' size={15} color='red' />
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  listItemView: {},
-  listItemText: {},
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
 });
 
 export default ListItem;
