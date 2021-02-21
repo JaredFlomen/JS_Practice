5777cc29df652d2ae44a2554e5517c638e2c115b
Jared Flomen <jaredflomen@gmail.com>
2021-02-21 14:52:45 -0500

    ListItem component

--- react_native/ShoppingList/App.js
+++ react_native/ShoppingList/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 import uuid from 'react-native-uuid';
 
 const App = () => {
@@ -13,6 +14,10 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} />}
+      />
     </View>
   );
 };
