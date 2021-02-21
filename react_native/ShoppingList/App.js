eb28d74d2b7739043534ed8d89f63139c9111953
Jared Flomen <jaredflomen@gmail.com>
2021-02-21 14:46:23 -0500

    Setting up flatlist

--- react_native/ShoppingList/App.js
+++ react_native/ShoppingList/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import uuid from 'react-native-uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Chicken' },
+    { id: uuid(), text: 'Eggs' },
+    { id: uuid(), text: 'Oreos' },
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
