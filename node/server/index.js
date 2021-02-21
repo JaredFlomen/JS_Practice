627f9468bf208a749d946723058c37396c3c3eab
Jared Flomen <jaredflomen@gmail.com>
2021-02-21 12:15:21 -0500

    Updates

--- node/server/index.js
+++ node/server/index.js
@@ -1,10 +1,13 @@
 //Simple web server
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from node express server!' });
+app.post('/dogs', (req, res) => {
+  console.log(req.body);
+  res.send('Jared');
 });
 
 app.listen(PORT, () => {
