ad9a7de10393b089c4f20451f1839280ec861944
Jared Flomen <jaredflomen@gmail.com>
2021-04-13 13:58:57 -0400

    Linting

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const App = props => {
+const App = ({ title }) => {
   return (
     <div>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       <h1>Hello From App</h1>
       <Link to='/profile'>Profile</Link>
     </div>
