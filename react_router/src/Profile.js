4fd28201ebbd85ca951ed8ecb3bb5f38c3921e7f
Jared Flomen <jaredflomen@gmail.com>
2021-04-13 13:59:23 -0400

    Linting

--- react_router/src/Profile.js
+++ react_router/src/Profile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Profile = props => {
+const Profile = ({ title }) => {
   return (
     <div>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       <h1>Hello from Profile</h1>
       <Link to='/'>Home</Link>
     </div>
